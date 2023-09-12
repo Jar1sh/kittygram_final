fc867f7122dc3c9b2c6ec101b83a9449fb62528d
Jar1sh <Jarrpen@yandex.ru>
2023-09-12 13:03:28 +0300

    Rework kittygram

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY", "django-settings-secret-key")
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-settings-secret-key')
 
-DEBUG = os.getenv('DEBUG', 'False').lower == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
