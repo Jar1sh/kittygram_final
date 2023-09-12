465d4566eb95c735444d49b52feca7313bc4fb4e
Jar1sh <Jarrpen@yandex.ru>
2023-09-12 16:50:21 +0300

    Kitty final

--- backend/setup/entrypoint.sh
+++ backend/setup/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py collectstatic
 cp -r /app/collected_static/. /backend_static/static/
+gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
